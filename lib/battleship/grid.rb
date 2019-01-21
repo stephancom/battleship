c6aa3521f5077ab81843f2be2eed5b6311e114ad
stephan.com <stephan@stephan.com>
2019-01-20 22:44:29 -0500

    add place ship methods

--- lib/battleship/grid.rb
+++ lib/battleship/grid.rb
@@ -1,4 +1,3 @@
-require_relative 'grid'
 module Battleship
   # the grid of cells
   class Grid
@@ -10,6 +9,33 @@ module Battleship
 
     def [](coord)
       @cells[coord.row][coord.col]
+    end
+
+    def place_ship_at(ship, coord, vertical)
+      place_ship_at!(ship, coord, vertical)
+      true
+    rescue CellOccupied, OutOfRange
+      false
+    end
+
+    def place_ship_at!(ship, coord, vertical)
+      # range checking is implicit here...
+      coords = if vertical
+                 Array.new(ship.size) { |delta_row| Coordinate.new(coord.row + delta_row, coord.col) }
+               else
+                 Array.new(ship.size) { |delta_col| Coordinate.new(coord.row, coord.col + delta_col) }
+               end
+      raise CellOccupied if coords.any? { |c| self[c].occupied? } # check before placing so there's no half-placed ships
+
+      coords.each do |c|
+        self[c].place_ship(ship)
+      end
+    end
+
+    def place_ship_randomly(ship)
+      loop do
+        break if place_ship_at(ship, Coordinate.random, [true, false].sample)
+      end
     end
   end
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Grid do
     end
   end
   describe 'the grid' do
-    let(:coords) { ('A'..'J').to_a.product((1..10).to_a).map(&:join).map { |str| Coordinate.new_from_string(str) } } # cartesian product FTW
+    let(:coords) { ('A'..'J').to_a.product((1..10).to_a).map(&:join).map { |str| Coordinate.from_string(str) } } # cartesian product FTW
     let(:grid) { Grid.new }
     let(:cells) { coords.map { |coord| grid[coord] } }
 
@@ -20,6 +20,145 @@ RSpec.describe Grid do
 
     it 'all cells should be unhit' do
       expect(cells).not_to include(be_hit)
+    end
+  end
+  describe 'when placing a ship' do
+    let(:ship) { Ship.new :submarine }
+    let(:grid) { Grid.new }
+    describe 'on an empty grid' do
+      describe 'in bounds' do
+        let(:start) { Coordinate.from_string('B2') }
+        [true, false].each do |vertical|
+          describe vertical ? 'vertically' : 'horizontally' do
+            let(:other_coords) { vertical ? %w[B3 B4] : %w[C2 D2] }
+            let(:coords_occupied) { [start] + other_coords.map { |c| Coordinate.from_string c } }
+            describe 'place_ship_at!' do
+              it 'should succeed' do
+                expect {
+                  grid.place_ship_at!(ship, start, vertical)
+                }.not_to raise_exception
+              end
+
+              it 'should call place_ship on the cells' do
+                coords_occupied.each do |c|
+                  expect(grid[c]).to receive(:place_ship).with(ship).and_call_original
+                end
+                grid.place_ship_at!(ship, start, vertical)
+              end
+
+              it 'should fill in the right coordinates' do
+                grid.place_ship_at!(ship, start, vertical)
+                expect(coords_occupied.map { |c| grid[c] }).to all(be_occupied)
+              end
+            end
+            describe 'place_ship_at' do
+              it 'should return true' do
+                expect(grid.place_ship_at(ship, start, vertical)).to be true
+              end
+
+              it 'should call place_ship_at!' do
+                expect(grid).to receive(:place_ship_at!).with(ship, start, vertical).and_call_original
+                grid.place_ship_at(ship, start, vertical)
+              end
+            end
+          end
+        end
+      end
+
+      describe 'out of bounds' do
+        let(:start) { Coordinate.from_string('I9') }
+        [true, false].each do |vertical|
+          describe vertical ? 'vertically' : 'horizontally' do
+            describe 'place_ship_at!' do
+              it 'should fail' do
+                expect {
+                  grid.place_ship_at!(ship, start, vertical)
+                }.to raise_exception(OutOfRange)
+              end
+            end
+            describe 'place_ship_at' do
+              it 'should return false' do
+                expect(grid.place_ship_at(ship, start, vertical)).to be false
+              end
+            end
+          end
+        end
+      end
+
+      describe 'place_ship_randomly' do
+        it 'should succeed' do
+          expect {
+            grid.place_ship_randomly(ship)
+          }.not_to raise_exception
+        end
+      end
+    end
+
+    [true, false].each do |vertical|
+      describe vertical ? 'vertically' : 'horizontally' do
+        let(:start) { Coordinate.from_string('D3') }
+
+        describe 'on a grid with other ships' do
+          let(:three_cell) { Ship.new(:cruiser) }
+          let(:four_cell) { Ship.new(:battleship) }
+          describe 'a safe distance away' do
+            before do
+              # vertical cruiser far enough from horizontal sub
+              grid.place_ship_at(three_cell, Coordinate.from_string('H2'), true)
+              # horizontal battleship far enough from vertical sub
+              grid.place_ship_at(four_cell, Coordinate.from_string('C7'), false)
+            end
+
+            describe 'place_ship_at!' do
+              it 'should succeed' do
+                expect {
+                  grid.place_ship_at!(ship, start, vertical)
+                }.not_to raise_exception
+              end
+            end
+            describe 'place_ship_at' do
+              it 'should return true' do
+                expect(grid.place_ship_at(ship, start, vertical)).to be true
+              end
+            end
+            describe 'place_ship_randomly' do
+              it 'should succeed' do
+                expect {
+                  grid.place_ship_randomly(ship)
+                }.not_to raise_exception
+              end
+            end
+          end
+          describe 'interfering with placement' do
+            before do
+              # vertical cruiser interferes with horizontal sub
+              grid.place_ship_at!(three_cell, Coordinate.from_string('E2'), true)
+              # horizontal battleship interferes with vertical sub
+              grid.place_ship_at!(four_cell, Coordinate.from_string('C5'), false)
+            end
+
+            describe 'place_ship_at!' do
+              it 'should fail' do
+                expect {
+                  grid.place_ship_at!(ship, start, vertical)
+                }.to raise_exception CellOccupied
+              end
+            end
+            describe 'place_ship_at' do
+              it 'should return false' do
+                expect(grid.place_ship_at(ship, start, vertical)).to be false
+              end
+            end
+            describe 'place_ship_randomly' do
+              it 'should succeed' do
+                expect {
+                  grid.place_ship_randomly(ship)
+                }.not_to raise_exception
+              end
+            end
+          end
+        end
+      end
     end
   end
 end
